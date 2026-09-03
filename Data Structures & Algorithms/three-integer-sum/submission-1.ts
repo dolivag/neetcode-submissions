6251a8770e518361eea1cfe7525e5cb490baf7f6
Dani Oliva <73400383+dolivag@users.noreply.github.com>
2026-09-03 09:37:33 +0200

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.ts
@@ -0,0 +1,44 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+ threeSum(nums: number[]): number[][] {
+        nums.sort((a, b) => a - b);
+        const result: number[][] = [];
+
+        for (let i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0) break;
+            
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+            const target = -nums[i];
+            let left = i + 1;
+            let right = nums.length - 1;
+
+            while (left < right) {
+                const sum = nums[left] + nums[right];
+                
+                if (sum === target) {
+                    result.push([nums[i], nums[left], nums[right]]);
+                    
+                    while (left < right && nums[left] === nums[left + 1]) {
+                        left++;
+                    }
+                    while (left < right && nums[right] === nums[right - 1]) {
+                        right--;
+                    }
+                    
+                    left++;
+                    right--;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return result;
+    }
+}
