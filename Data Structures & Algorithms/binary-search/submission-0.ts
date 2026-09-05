fb7338815ac7fae452e2ce7b24246e1f35d819d8
Dani Oliva <73400383+dolivag@users.noreply.github.com>
2026-09-05 21:41:05 +0200

    Add: binary-search - submission-0

---
+++ Data Structures & Algorithms/binary-search/submission-0.ts
@@ -0,0 +1,12 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number}
+     */
+    search(nums: number[], target: number): number {
+        return nums.findIndex((num)=>{
+           return num ===  target
+        });
+    }
+}
