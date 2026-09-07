a0ea253dd8de1631b9f45ca2b141c3744d3fe8ac
Dani Oliva <73400383+dolivag@users.noreply.github.com>
2026-09-07 10:39:15 +0200

    Add: buy-and-sell-crypto - submission-6

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-6.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} prices
+     * @return {number}
+     */
+    maxProfit(prices: number[]): number {
+        let maxPrice: number = 0;
+        let left: number = 0;
+        let right: number = 1;
+        
+        while (right <= prices.length && left !== right) {
+            if (prices[right] > prices[left]) {
+                maxPrice = Math.max(maxPrice, prices[right]- prices[left]);
+            } else {
+                left = right
+            }
+            right++;
+        }
+        return maxPrice;
+    }
+}
